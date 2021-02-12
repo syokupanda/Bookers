eb5a1c9ccc4d5ca3b8b2d1126afb30b75e0501d9
syokupanda <borun.books@gmail.com>
2021-02-12 14:18:23 +0000

    edit/destroy complete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,6 +33,12 @@ class BooksController < ApplicationController
     redirect_to bookshow_path(book.id)
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
   # ストロングパラメータ
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get 'books/:id' => 'books#show', as: 'bookshow'
   get 'books/:id/edit' => 'books#edit', as: 'edit_bookshow'
   patch 'books/:id' => 'books#update', as: 'update_bookshow'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_bookshow'
 end
